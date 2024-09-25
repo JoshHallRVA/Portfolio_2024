808d0985004827dfdb0c25af239d616848641ffb
josh hall <joshhall@iMac.local>
2024-09-25 18:20:19 -0400

    resolved

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import AppHeader from './components/header';
+import AppHero from './components/hero';
+import AppAbout from './components/about';
+import AppServices from './components/services';
+import AppWorks from './components/works';
+import AppTeams from './components/teams';
+import AppTestimonials from './components/testimonials';
+import AppPricing from './components/pricing';
+import AppBlog from './components/blog';
+import AppContact from './components/contact';
+import AppFooter from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      <header id="header">
+      <header id='header'>
         <AppHeader />
       </header>
+      <main>
+        <AppHero />
+        <AppAbout />
+        <AppServices />
+        <AppWorks />
+        <AppTeams />
+        <AppTestimonials />
+        <AppPricing />
+        <AppBlog />
+        <AppContact />
+      </main>
+      <footer id="footer">
+        <AppFooter />
+      </footer>
     </div>
   );
 }
